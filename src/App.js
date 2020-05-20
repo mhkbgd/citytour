68c0cf7b0c559504fa7a3bc94191442bd731f47d
makhan <makhan@ttu.ee>
2020-05-20 19:20:01 +0300

    Home page added with basic CSS!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <div className="App-body">
+          <h1>Welcome to City Tour Booking</h1>
+
+          <div class="row">
+            <div class="column">
+            <button><img src="./helsinki.jpg" alt="my image" />Helsinki</button>
+            
+            </div>
+            <div class="column">
+            <button><img src="./tampere.jpg" alt="my image" /> Tampere</button>
+            </div>
+            <div class="column">
+            <button><img src="./turku.jpg" alt="my image" />Turku</button>
+            </div>
+          </div>
+            
+          <div class="row">
+            <div class="column">
+            <button><img src="./oulu.jpg" alt="my image" />Oulu</button>
+            </div>
+            <div class="column">
+            <button><img src="./kuopio.jpg" alt="my image" />Kuopio</button>
+            </div>
+            <div class="column">
+            <button><img src="./joensuu.jpg" alt="my image" />Joensuu</button>
+            </div>
+          </div>
+            
+
+          <script src="/__/firebase/7.14.4/firebase-app.js"></script>
+          <script src="/__/firebase/7.14.4/firebase-analytics.js"></script>
+          <script src="/__/firebase/init.js"></script>
+      </div>
+      
     </div>
+
+
+
   );
 }
 
