42631244d716c743d20ffe8e254974e9a1822aa6
makhan <makhan@ttu.ee>
2020-05-21 14:40:56 +0300

    updated form!

--- src/components/form.js
+++ src/components/form.js
@@ -1,70 +1,61 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 export default class Form extends React.Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    city: "",
-    email: ""
-  };
-
-  change = e => {
-    this.props.onChange({ [e.target.name]: e.target.value });
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-    // this.props.onSubmit(this.state);
-    this.setState({
-      firstName: "",
-      lastName: "",
-      city: "",
-      email: ""
-    });
-    this.props.onChange({
-      firstName: "",
-      lastName: "",
-      city: "",
-      email: ""
-    });
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      mobile: null,
+      city: '',
+    };
+  }
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    let mobile = this.state.mobile;
+    if (this.state.username == '') {
+      alert("Please enter name!");
+    }
+    if (!Number(mobile)) {
+      alert("Your mobile must be a number");
+    }else{
+      alert("Thank you! Our operator will contact soon");
+      
+      
+    }
+  }
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
   render() {
     return (
-      <form>
-        <input
-          name="firstName"
-          placeholder="First name"
-          value={this.state.firstName}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name="lastName"
-          placeholder="Last name"
-          value={this.state.lastName}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name="city"
-          placeholder="City"
-          value={this.state.city}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name="email"
-          placeholder="Email"
-          value={this.state.email}
-          onChange={e => this.change(e)}
-        />
-        
-        <br />
-        <button onClick={e => this.onSubmit(e)}>Submit</button>
+      <form onSubmit={this.mySubmitHandler}>
+      <h1>Hello {this.state.username}</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        name='username'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter your mobile number:</p>
+      <input
+        type='text'
+        name='mobile'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter City</p>
+      <input
+        type='text'
+        name='city'
+        onChange={this.myChangeHandler}
+      />
+
+      
+      <br/>
+      <input type='submit' />
       </form>
     );
   }
